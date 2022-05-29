ea17487b13381dae4e3f44bc4c3e0832a18c9af9
Saul Tovar <tovarsaul@gmail.com>
2022-05-29 16:40:47 +0200

    add db conection

---
+++ bd/conection.go
@@ -0,0 +1,34 @@
+package bd
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+var MongoDBConnection = Connect()
+var clientOptions = options.Client().ApplyURI("mongodb+srv://saul:dT9aGdYN8sDhT00s@twitter.vmccd.mongodb.net/?retryWrites=true&w=majority")
+
+func Connect() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	log.Println("Connection successful")
+	return client
+}
+
+func CheckConection() int {
+	err := MongoDBConnection.Ping(context.TODO(), nil)
+	if err != nil {
+		return 0
+	}
+	return 1
+}
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"twittergo/bd"
 
+	"twittergo/handlers"
+)
+
+func main() {
+	if bd.CheckConection() == 0 {
+		log.Fatal("Error al conectar a la bbdd")
+		return
+	}
+	handlers.Handlers()
+	log.Println("Hola")
 }
